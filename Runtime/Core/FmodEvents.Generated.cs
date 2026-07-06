9f51e2d0df97cdf0ba8125233762bba7cd05a87d
NatanVanique <nvanique.o@gmail.com>
2026-07-06 11:02:08 -0300

    Tirando plataformas desnecessarias

--- Runtime/Core/FmodEvents.Generated.cs
+++ Runtime/Core/FmodEvents.Generated.cs
@@ -1,8 +1,12 @@
-#if FMOD_PRESENT
+﻿#if FMOD_PRESENT
 /// <summary>
 /// Fornece ids de eventos BetterFMOD gerados automaticamente.
 /// </summary>
 public static class FmodEvents
 {
+    /// <summary>
+    /// Id de evento BetterFMOD: Test
+    /// </summary>
+    public const string Test = "Test";
 }
 #endif
